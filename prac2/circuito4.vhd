b0d8581e4326875fc7fe273d049a2a28120947ec
Alan Acosta <alanomar460@gmail.com>
2025-08-25 22:48:20 -0600

    Update circuito4.vhd

--- prac2/circuito4.vhd
+++ prac2/circuito4.vhd
@@ -4,13 +4,13 @@ USE ieee.std_logic_1164.ALL;
 ENTITY circuito IS
 	PORT (
 		A, B, C, D : IN STD_LOGIC;
-		Y1, Y2     : OUT STD_LOGIC
+		Y2, Y3     : OUT STD_LOGIC
     );
 END circuito;
 
 ARCHITECTURE behavior OF circuito IS
 BEGIN
-	Y1 <= (A OR B OR C OR D) AND
+	Y2 <= (A OR B OR C OR D) AND
 			(A OR B OR NOT C OR D) AND
 			(A OR NOT B OR C OR D) AND
 			(A OR NOT B OR NOT C OR D) AND
@@ -18,5 +18,5 @@ BEGIN
 			(NOT A OR B OR NOT C OR D) AND
 			(NOT A OR B OR NOT C OR NOT D);
 	
-	Y2 <= (NOT A AND D) OR (A AND B) OR (NOT C AND D);
+	Y3 <= (NOT A AND D) OR (A AND B) OR (NOT C AND D);
 END behavior;
