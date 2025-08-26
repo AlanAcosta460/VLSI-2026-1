ee8e2d21dae86151c7cb8f1084e924647f4a0f61
Alan Acosta <alanomar460@gmail.com>
2025-08-25 20:58:14 -0600

    Update circuito1.vhd

--- prac2/circuito1.vhd
+++ prac2/circuito1.vhd
@@ -4,7 +4,7 @@ USE ieee.std_logic_1164.ALL;
 ENTITY circuito1 IS
 	PORT (
 		A, B   : IN STD_LOGIC;
-		W1, W2 : OUT STD_LOGIC
+		W1, W3 : OUT STD_LOGIC
     );
 END circuito1;
 
@@ -13,5 +13,5 @@ BEGIN
 	W1 <= (NOT A AND B) OR
 			(A AND B);
 		
-	W2 <= B;
+	W3 <= B;
 END behavior;
