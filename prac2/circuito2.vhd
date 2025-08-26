2b900c95dece49524de74610dcb517d3c5779101
Alan Acosta <alanomar460@gmail.com>
2025-08-25 22:00:40 -0600

    Update circuito2.vhd

--- prac2/circuito2.vhd
+++ prac2/circuito2.vhd
@@ -4,15 +4,15 @@ USE ieee.std_logic_1164.ALL;
 ENTITY circuito2 IS
 	PORT (
 		X, Y, Z   : IN STD_LOGIC;
-		S3, S4 : OUT STD_LOGIC
+		S2, S3 : OUT STD_LOGIC
     );
 END circuito2;
 
 ARCHITECTURE behavior OF circuito2 IS
 BEGIN
-	S3 <= (X OR Y OR Z) AND
+	S2 <= (X OR Y OR Z) AND
 			(X OR Y OR NOT Z) AND
 			(NOT X OR Y OR Z);
 		
-	S4 <= Y OR (X AND Z);
+	S3 <= Y OR (X AND Z);
 END behavior;
