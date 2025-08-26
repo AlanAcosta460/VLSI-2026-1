8d2337bcdaee844958da43f4b722e02b2242fa6a
Alan Acosta <alanomar460@gmail.com>
2025-08-25 22:13:19 -0600

    Update circuito3.vhd

--- prac2/circuito3.vhd
+++ prac2/circuito3.vhd
@@ -4,15 +4,15 @@ USE ieee.std_logic_1164.ALL;
 ENTITY circuito3 IS
 	PORT (
 		B, C, D : IN STD_LOGIC;
-		Y1, Y2  : OUT STD_LOGIC
+		Y2, Y3  : OUT STD_LOGIC
     );
 END circuito3;
 
 ARCHITECTURE behavior OF circuito3 IS
 BEGIN
-	Y1 <= (B OR C OR D) AND
+	Y2 <= (B OR C OR D) AND
 			(B OR C OR NOT D) AND
 			(B OR NOT C OR D);
 	
-	Y2 <= B OR (C AND D);
+	Y3 <= B OR (C AND D);
 END behavior;
